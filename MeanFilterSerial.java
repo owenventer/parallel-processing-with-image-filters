f9859c7b501ff507f32c03c88f8e879b52354e3a
Owen <VNTNIC019@myuct.ac.za>
2022-08-07 21:06:56 +0200

    Skeleton code for reading and writing to an image

--- MeanFilterSerial.java
+++ MeanFilterSerial.java
@@ -1,53 +1,61 @@
 //imports
 import java.io.File;
 import java.io.IOException;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class MeanFilterSerial{
-    //comment
     public static void main(String args[])throws IOException{
-        BufferedImage img = null;
-        File f = null;
+      
+      //Reading in the image
+      BufferedImage img = null;
+      File file = null;
+      //try/catch in case of errors
+      try{
+        file= new File("Sample.jpg");
+        img = ImageIO.read(file);
+      }catch(IOException e){
+        System.out.println("ERROR:"+e);
+      }
+      //storing the height and width of the current image
+      int height=img.getHeight();
+      int width=img.getWidth();
+      for (int y = 0; y < height; y++) {
+         for (int x = 0; x < width; x++) {
+
+            //Pulling in data about the current pixel
+            int pixel = img.getRGB(x,y);
+
+            //Creating a colour from the current pixel
+            Color color = new Color(pixel, true);
+
+            //Getting RGB from current pixel 
+            int r = color.getRed();
+            int g = color.getGreen();
+            int b = color.getBlue();
+
+            //Code to change the pixels that are to be written
+            g = 150;
+            b = 150;
+
+            //Creating a new colour from the updated RGB values
+            color = new Color(r, g, b);
+
+            //updating the image with the new pixel colour
+            img.setRGB(x, y, color.getRGB());
+
+         }
+      }
+      //Save the image, use try/catch for errors
+      try{
+        file = new File("/Users/owen/OneDrive - University of Cape Town/My UCT/2nd Year/CSC2002S/Week 2/Assignment 1/Output.jpg");
+        ImageIO.write(img, "jpg", file);
+        System.out.println("Finished.");
+      }catch(IOException e){
+      System.out.println("ERROR:"+e);
     
-        //read image
-        try{
-          f = new File("Sample.jpg");
-          img = ImageIO.read(f);
-        }catch(IOException e){
-          System.out.println(e);
-        }
-    
-        // some code goes here...
-        int width = img.getWidth();
-        int height = img.getHeight();
-
-        //Getting the values
-        int getP = img.getRGB(0,0);
-        //get alpha
-        int getA = (getP>>24) & 0xff;
-        int getR = (getP>>16) & 0xff;
-        int getG = (getP>>8) & 0xff;
-        int getB = getP & 0xff;
-
-        //Setting the new values
-       
-        int setA = 255;
-        int setR = 100;
-        int setG = 150;
-        int setB = 200;
-
-        //set the pixel value
-        int setP = (setA<<24) | (setR<<16) | (setG<<8) | setB;
-        img.setRGB(0, 0, setP);
-
-        //write image
-        try{
-            f = new File("/Output.jpg");
-            ImageIO.write(img, "jpg", f);
-        }catch(IOException e){
-            System.out.println(e);
-
-      }//main() ends here
 }
+
+}//main() ends here
 }
--- MedianFilterParallel.java
+++ MedianFilterParallel.java
@@ -1,3 +1,61 @@
+//imports
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 public class MedianFilterParallel {
-    
+
+    public static void main(String args[])throws IOException{
+      
+        //Reading in the image
+        BufferedImage img = null;
+        File file = null;
+        //try/catch in case of errors
+        try{
+          file= new File("Sample.jpg");
+          img = ImageIO.read(file);
+        }catch(IOException e){
+          System.out.println("ERROR:"+e);
+        }
+        //storing the height and width of the current image
+        int height=img.getHeight();
+        int width=img.getWidth();
+        for (int y = 0; y < height; y++) {
+           for (int x = 0; x < width; x++) {
+  
+              //Pulling in data about the current pixel
+              int pixel = img.getRGB(x,y);
+  
+              //Creating a colour from the current pixel
+              Color color = new Color(pixel, true);
+  
+              //Getting RGB from current pixel 
+              int r = color.getRed();
+              int g = color.getGreen();
+              int b = color.getBlue();
+  
+              //Code to change the pixels that are to be written
+              g = 150;
+              b = 150;
+  
+              //Creating a new colour from the updated RGB values
+              color = new Color(r, g, b);
+  
+              //updating the image with the new pixel colour
+              img.setRGB(x, y, color.getRGB());
+  
+           }
+        }
+        //Save the image, use try/catch for errors
+        try{
+          file = new File("/Users/owen/OneDrive - University of Cape Town/My UCT/2nd Year/CSC2002S/Week 2/Assignment 1/Output.jpg");
+          ImageIO.write(img, "jpg", file);
+          System.out.println("Finished.");
+        }catch(IOException e){
+        System.out.println("ERROR:"+e);
+      
+  }
+  
+  }//main() ends here
 }
